86db9dd2c621c900ba884eb23935d4cb3261e9cc
UJJY <ujj.aggarwal@gmail.com>
2020-05-08 02:30:51 +0400

    Added test objects

--- project1.py
+++ project1.py
@@ -1,2 +1,5 @@
 #THE BORDERLESS TOURIST
 
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+test_traveler = ['Erin Wikes', 'Shaghai, China', ['historical site', 'art']]
+
