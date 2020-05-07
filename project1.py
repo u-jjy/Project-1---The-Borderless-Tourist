f04fd276cedbb7837074f782ccb6481e28aa3a1d
UJJY <ujj.aggarwal@gmail.com>
2020-05-08 02:58:36 +0400

    Added logic to find traveler destinations and convert internal data

--- project1.py
+++ project1.py
@@ -1,5 +1,21 @@
 #THE BORDERLESS TOURIST
 
 destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
-test_traveler = ['Erin Wikes', 'Shaghai, China', ['historical site', 'art']]
+test_traveler = ['Erin Wikes', 'Shanghai, China', ['historical site', 'art']]
 
+def get_destination_index(destination):
+    destination_index = None
+    for i in destinations:
+        if i == destination:
+            destination_index = destinations.index(i)
+    return destination_index
+
+print(get_destination_index("Los Angeles, USA"))
+
+def get_traveler_location(traveler):
+    traveler_destination = traveler[1]
+    traveler_destination_index = get_destination_index(traveler_destination)
+    return traveler_destination_index
+
+test_destination_index = get_traveler_location(test_traveler)
+print(test_destination_index)
